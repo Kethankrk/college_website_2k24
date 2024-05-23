14a6201a6ac36c204a2c7bcee6b77cac8708bb0d
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-05-23 22:39:24 +0530

    Added hero , nav ,top

--- app/components/homepage/herosection.jsx
+++ app/components/homepage/herosection.jsx
@@ -1,7 +1,123 @@
+"use client";
 import React from "react";
+import { Carousel, Typography, Button } from "@material-tailwind/react";
 
 const Herosection = () => {
-  return <div className=""></div>;
+  return (
+    <div className="w-full ">
+      <Carousel className=" h-[500px] " loop autoplay autoplayDelay={8000}>
+        <div className="relative h-full w-full">
+          <img
+            src="https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image 1"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+            <div className="w-3/4 text-center md:w-2/4">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                Welcome to <br />
+                COLLEGE OF APPLIED SCIENCE THAMARSSERY
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex justify-center gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-full w-full">
+          <img
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            alt="image 2"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+            <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                The Beauty of Nature
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-full w-full">
+          <img
+            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+            alt="image 3"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+            <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                The Beauty of Nature
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Herosection;
--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,14 @@ import ContactSection from "./components/homepage/contactsection";
 import FacilitySection from "./components/homepage/facilitysection";
 import FooterSection from "./components/homepage/footersection";
 import QuestionSection from "./components/homepage/questionSection";
+import Herosection from "./components/homepage/herosection";
 
 export default function Home() {
   return (
     <main className="">
       <Topsection />
       <Navbar />
-      <p>Homepage</p>
+      <Herosection />
       <FacilitySection />
       <QuestionSection />
       <ContactSection />
