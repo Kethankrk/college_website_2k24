461b04b8ea1c74a4d85f9aa23a0a05d61c56489e
Kethankrk <kethankrk@gmail.com>
2024-05-23 21:23:59 +0530

    added old components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
+import ContactSection from "./components/homepage/contactsection";
+import FacilitySection from "./components/homepage/facilitysection";
+import FooterSection from "./components/homepage/footersection";
+import QuestionSection from "./components/homepage/questionSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <p>Homepage</p>
+      <FacilitySection />
+      <QuestionSection />
+      <ContactSection />
+      <FooterSection />
     </main>
   );
 }
