2e56871c9ab1fa59b37bb9fb2ba92dca1b40a95c
Kethankrk <kethankrk@gmail.com>
2024-05-24 10:41:24 +0530

    added proper title to profile menu

--- app/(home)/components/homepage/navbar.jsx
+++ app/(home)/components/homepage/navbar.jsx
@@ -43,19 +43,19 @@ export default Navbar;
 const ProfileMenu = () => {
   const menuItems = [
     {
-      title: "@material-tailwind/html",
+      title: "About us",
+      description:
+        "A complete set of UI Elements for building faster websites in less time.",
+    },
+    {
+      title: "Management",
       description:
         "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
     },
     {
-      title: "@material-tailwind/react",
+      title: "Administration",
       description:
         "Learn how to use @material-tailwind/react, packed with rich components for React.",
-    },
-    {
-      title: "Material Tailwind PRO",
-      description:
-        "A complete set of UI Elements for building faster websites in less time.",
     },
   ];
   const [openMenu, setOpenMenu] = React.useState(false);
