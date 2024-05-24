e1173e48d341e7e594884d1d850c0ca6dffff6e1
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-05-24 13:23:22 +0530

    "Updated Topsection component: adjusted CSS classes, icon sizes, and added responsive design elements."

--- app/(home)/components/homepage/topsection.jsx
+++ app/(home)/components/homepage/topsection.jsx
@@ -7,23 +7,25 @@ import {
   FaLinkedin,
   FaYoutube,
 } from "react-icons/fa";
+
 const Topsection = () => {
   return (
-    <div className="px-12 py-4  justify-between sm:flex w-full bg-[#0F4077] text-white hidden ">
-      <div className=" flex gap-6">
-        <p className="flex gap-1 items-center">
-          <MdOutlinePhoneAndroid className="w-5 h-5" />
+    <div className="px-4 py-2 sm:px-12 sm:py-4 flex flex-wrap justify-between items-center bg-[#0F4077] text-white">
+      <div className="flex gap-4 md:gap-6 items-center flex-wrap">
+        <p className="flex gap-1 items-center text-xs sm:text-sm md:text-base">
+          <MdOutlinePhoneAndroid className="w-4 h-4 sm:w-5 sm:h-5" />
           0495 123 123 123
         </p>
-        <p className="flex gap-1 items-center">
-          <IoIosMail className="w-6 h-6" /> casthamarassery@email.com
+        <p className="flex gap-1 items-center text-xs sm:text-sm md:text-base">
+          <IoIosMail className="w-5 h-5 sm:w-6 sm:h-6" />
+          casthamarassery@email.com
         </p>
       </div>
-      <div className="flex gap-2">
-        <FaInstagram />
-        <FaFacebookSquare />
-        <FaLinkedin />
-        <FaYoutube />
+      <div className="flex gap-2 mt-2 sm:mt-0">
+        <FaInstagram className="w-4 h-4 sm:w-5 sm:h-5" />
+        <FaFacebookSquare className="w-4 h-4 sm:w-5 sm:h-5" />
+        <FaLinkedin className="w-4 h-4 sm:w-5 sm:h-5" />
+        <FaYoutube className="w-4 h-4 sm:w-5 sm:h-5" />
       </div>
     </div>
   );
